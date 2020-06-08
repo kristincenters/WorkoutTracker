c2fe2a0f3e49115b706ae8c6bccfca9150e620f5
kcenters <kristincenters@gmail.com>
2020-06-07 20:43:16 -0500

    working routes

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,7 @@
+// Add exercises to a previous workout plan.
+// Add new exercises to a new workout plan.
+// View multiple the combined weight of multiple exercises on the stats page.
+
 const db = require('../models');
 console.log(db.Workout);
 const express = require('express');
@@ -13,4 +17,38 @@ router.get('/api/workout', (req, res) => {
 			res.json(err);
 		});
 });
+router.post('/api/workout', (req, res) => {
+	console.log('hello');
+	db.Workout.create({})
+		.then((workout) => {
+			res.json(workout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+router.put('/api/workout/:id', (req, res) => {
+	db.Workout.findOneAndUpdate(
+		{ _id: req.params.id },
+		{ $push: { exercises: req.body } }
+	)
+		.then((workout) => {
+			res.json(workout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.get('/api/workout/range', (req, res) => {
+	db.Workout.find({})
+		// .populate('exercises')
+		.then((workout) => {
+			res.json(workout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 
 // routes
 app.use(require('./routes/api'));
-// app.use(require('./routes/html'));
+app.use(require('./routes/html'));
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
